3787986813e006a22086531407d7a3b4b6300ebc
Aka <163530203+Aka871@users.noreply.github.com>
2026-08-27 18:13:48 +0900

    feat: 受講生の登録と一覧表示を実装

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,38 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
+    List<Student> students = new ArrayList<>();
+    
+// インスタンスが作られた時点で、studentIdは作られているので、コンストラクタの引数で渡す必要はない
+    students.add(new Student(
+        "山田太郎",
+        "090-1234-5678",
+        "yamada@example.com",
+        "Javaコース",
+        LocalDate.of(2026, 8, 1),
+        "平日は18時以降に連絡希望"));
 
+    students.add(new Student(
+        "佐藤花子",
+        "080-9876-5432",
+        "sato@example.com",
+        "Webコース",
+        LocalDate.of(2026, 8, 15),
+        "メールでの連絡希望"
+    ));
+
+// System.out.println(students.toString()); という書き方もできるが、受講生一覧表示ならfor文の方が見やすく、自然。
+// println() にオブジェクトを渡すと、内部的にそのオブジェクトの toString() が使われる。
+// System.out.println(student.toString());のtoString()は省略できるが、今は勉強のため書いておく。
+
+    for (Student student : students) {
+      System.out.println(student.toString());
+    }
   }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Student {
+
+  private UUID studentId = UUID.randomUUID();
+
+  private String studentName;
+  private String phoneNumber;
+  private String emailAddress;
+  private String courseName;
+  private LocalDate courseStartDate;
+  private String remark;
+
+  public Student(String studentName, String phoneNumber, String emailAddress,
+      String courseName, LocalDate courseStartDate, String remark) {
+    this.studentName = studentName;
+    this.phoneNumber = phoneNumber;
+    this.emailAddress = emailAddress;
+    this.courseName = courseName;
+    this.courseStartDate = courseStartDate;
+    this.remark = remark;
+  }
+
+  @Override
+  public String toString() {
+    return "受講生ID：" + this.studentId
+        + " / 氏名：" + this.studentName
+        + " / 電話番号：" + this.phoneNumber
+        + " / メールアドレス：" + this.emailAddress
+        + " / 受講コース：" + this.courseName
+        + " / コース開始日：" + this.courseStartDate
+        + " / 備考：" + this.remark;
+  }
+}
